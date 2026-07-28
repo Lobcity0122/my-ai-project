8cd621e06b4e80463ece59348884e25a85322ff0
lobcity <asasaaxscscscs@gmail.com>
2026-07-28 09:57:45 +0900

    コメント追加

--- skinned_mesh.h
+++ skinned_mesh.h
@@ -9,17 +9,21 @@
 
 using namespace Microsoft::WRL;
 
-// ゲームエンジン側で保持する独自シーン・ノード構造体
+// シーン全体、ノード階層を管理する構造体
 struct scene
 {
+	// FBXシーンのノード(ボーン、メッシュ、ライト等)を表す構造体
 	struct node
 	{
-		uint64_t unique_id{ 0 };
-		std::string name;
-		FbxNodeAttribute::EType attribute{ FbxNodeAttribute::EType::eUnknown };
-		int64_t parent_index{ -1 };
+		uint64_t unique_id{ 0 };                        // ノード固有の識別ID
+		std::string name;                               // ノード名
+		FbxNodeAttribute::EType attribute{ FbxNodeAttribute::EType::eUnknown }; // 属性タイプ（メッシュ、ボーン等）
+		int64_t parent_index{ -1 };                     // 親ノードの配列インデックス（ルートの場合は -1）
 	};
+
 	std::vector<node> nodes;
+
+	// 指定したUniqueIDからノード配列のインデックスを探索するヘルパー関数
 	int64_t indexof(uint64_t unique_id) const
 	{
 		int64_t index{ 0 };
@@ -32,20 +36,23 @@ struct scene
 
 			++index;
 		}
-		return -1;
+		return -1; // 見つからない場合は－1
 	}
 };
 
+// スキンメッシュ(3Dキャラクター等)を管理、描画するクラス
 class skinned_mesh
 {
 public:
+	// 頂点データ構造体(位置、法線、テクスチャ座標)
 	struct vertex
 	{
-		DirectX::XMFLOAT3 position;
-		DirectX::XMFLOAT3 normal{ 0, 1, 0 };
-		DirectX::XMFLOAT2 texcoord{ 0, 0 };
+		DirectX::XMFLOAT3 position;                     // 頂点座標
+		DirectX::XMFLOAT3 normal{ 0, 1, 0 };            // 法線ベクトル
+		DirectX::XMFLOAT2 texcoord{ 0, 0 };             // UV座標
 	};
 
+	// 定数バッファ用構造体(ワールド行列やマテリアルカラー等)
 	struct constants
 	{
 		DirectX::XMFLOAT4X4 world;
@@ -59,9 +66,10 @@ private:
 	Microsoft::WRL::ComPtr<ID3D11Buffer> constant_buffer;
 
 public:
+	// コンストラクタ：FBXファイルを読み込み、シーンやノードツリーを構築する
 	skinned_mesh(ID3D11Device * device, const char* fbx_filename, bool triangulate = false);
 	virtual ~skinned_mesh() = default;
 
 protected:
-	scene scene_view;
+	scene scene_view; // シーンビュー(ノード階層情報)
 };
